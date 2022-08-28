0fe61746828265116219898fd6c3201e15a1616d
Sebagadis Kahsay <sebexk3@gmail.com>
2022-08-28 08:03:38 +0300

    Add nested If statement

--- JavaGradeProgram/src/Grade.java
+++ JavaGradeProgram/src/Grade.java
@@ -16,28 +16,49 @@ public class Grade {
         System.out.print("==> ");
         double mark = scan.nextDouble();
 
-        if (mark >= 90) {
+        if (mark<=100 && mark >= 90) {
             System.out.println("Grade A+");
-        } else if (mark < 90 && mark >= 85) {
-            System.out.println("Grade A");
-        } else if (mark < 85 && mark >= 80) {
-            System.out.println("Grade A-");
-        } else if (mark < 80 && mark >= 75) {
-            System.out.println("Grade B+");
-        } else if (mark < 75 && mark >= 70) {
-            System.out.println("Grade B");
-        } else if (mark < 70 && mark >= 65) {
-            System.out.println("Grade B-");
-        } else if (mark < 65 && mark >= 60) {
-            System.out.println("Grade C+");
-        } else if (mark < 60 && mark >= 50) {
-            System.out.println("Grade C");
-        } else if (mark < 50 && mark >= 45) {
-            System.out.println("Grade C-");
-        } else if (mark < 45 && mark >= 40) {
-            System.out.println("Grade D");
         } else {
-            System.out.println("Failed! Grade F");
+            if (mark >= 85) {
+                System.out.println("Grade A");
+            } else {
+                if (mark >= 80) {
+                    System.out.println("Grade A-");
+                } else {
+                    if (mark >= 75) {
+                        System.out.println("Grade B+");
+                    } else {
+                        if (mark >= 70) {
+                            System.out.println("Grade B");
+                        } else {
+                            if (mark >= 65) {
+                                System.out.println("Grade B-");
+                            } else {
+                                if (mark >= 60) {
+                                    System.out.println("Grade C+");
+                                } else {
+                                    if (mark >= 50) {
+                                        System.out.println("Grade C");
+                                    } else {
+                                        if (mark >= 45) {
+                                            System.out.println("Grade C-");
+                                        } else {
+                                            if (mark >= 40) {
+                                                System.out.println("Grade D");
+                                            } else {
+                                                if (mark <40 && mark >=0)
+                                                System.out.println("Failed! Grade F");
+                                            } else {
+                                                System.out.println("Invalid Input");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
